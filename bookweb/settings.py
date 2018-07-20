0fde8c7bfbb14536bc0009ede44fc3cdefd11df5
Quang <quangdangfit.com>
2018-07-20 10:02:51 +0700

    Fix database v2

--- bookweb/settings.py
+++ bookweb/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
